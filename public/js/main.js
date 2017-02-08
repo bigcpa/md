43797f9158f0ae7917282e62f8df1b63aff6ebe8
Alex Creio <mailcreio@gmail.com>
2017-02-08 12:53:40 +0000

    Updated 'public/js/main.js' via CloudCannon

--- public/js/main.js
+++ public/js/main.js
@@ -16,28 +16,18 @@
 })(document);
 
 // scrolljump
-window.onload = function() { // после загрузки страницы
-	var scrollUp = document.getElementById('scrollup'); // найти элемент
-	scrollUp.onmouseover = function() { // добавить прозрачность
-		scrollUp.style.opacity=1;
-		scrollUp.style.filter  = 'alpha(opacity=100)';
-	};
-	scrollUp.onmouseout = function() { //убрать прозрачность
-		scrollUp.style.opacity = 0.5;
-		scrollUp.style.filter  = 'alpha(opacity=50)';
-	};
-	scrollUp.onclick = function() { //обработка клика
-		window.scrollTo(0,0);
-	};
-// show button
-	window.onscroll = function () { // при скролле показывать и прятать блок
-		if ( window.pageYOffset > 0 ) {
-			scrollUp.style.display = 'block';
-		} else {
-			scrollUp.style.display = 'none';
-		}
-	};
-};
+$(function() { 
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+      $('#scrollup').fadeIn();
+    } else {
+      $('#scrollup').fadeOut();
+    }
+  });
+  $('#scrollup').click(function() {
+    $('body,html').animate({scrollTop:0},800);
+  });
+});
 
 
 // chatra settings
