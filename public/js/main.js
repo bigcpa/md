b38ebcef9e8ad99a89c53da13e2651c5c6fe32e9
Alex Creio <mailcreio@gmail.com>
2017-02-08 03:55:21 +0000

    Updated 'public/js/main.js' via CloudCannon

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
+// sidebar-toggle
+(function(document) {
+	var toggle = document.querySelector('.sidebar-toggle');
+	var sidebar = document.querySelector('#sidebar');
+	var checkbox = document.querySelector('#sidebar-checkbox');
+
+	document.addEventListener('click', function(e) {
+		var target = e.target;
+
+		if(!checkbox.checked ||
+			sidebar.contains(target) ||
+			(target === checkbox || target === toggle)) return;
+
+			checkbox.checked = false;
+	}, false);
+})(document);
+
 // scrolljump
 
 window.onload = function() { // после загрузки страницы
