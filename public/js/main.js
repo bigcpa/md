490b3b455593271f52fb4efcdafc5c0444766525
Alex Creio <mailcreio@gmail.com>
2017-02-13 14:46:14 +0000

    Updated 'public/js/main.js' via CloudCannon

--- public/js/main.js
+++ public/js/main.js
@@ -55,11 +55,6 @@ $('#fixblock').scroolly([
 
 
 
-// pjax
-// $(document).pjax('a', '#pjax-container')
-
-
-
 // scrolljump
 $(function() { 
   $(window).scroll(function() {
@@ -80,11 +75,11 @@ $(function() {
 (function($) {
 $(function() {
 
-  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-    $(this)
-      .addClass('active').siblings().removeClass('active')
-      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-  });
+	$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+		$(this)
+			.addClass('active').siblings().removeClass('active')
+			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+	});
 
 });
 })(jQuery);
